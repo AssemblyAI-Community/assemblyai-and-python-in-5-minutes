829cc280f78ddebe309f338fad75b5acf5261ce0
Ryan O <46252299+oconnoob@users.noreply.github.com>
2022-04-22 14:13:55 -0400

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -4,22 +4,22 @@ api_key = "<YOUR-API-KEY>"
 audio_file = "audio.mp3"
 
 # Create header with authorization along with AssemblyAI API requests
-headers = make_headers(api_key)
+header = make_header(api_key)
 
 # Upload the audio file to AssemblyAI
-upload_url = upload_file(audio_file, headers)
+upload_url = upload_file(audio_file, header)
 
 # Request a transcription
-transcript_response = request_transcript(upload_url, headers)
+transcript_response = request_transcript(upload_url, header)
 
 # Create a polling endpoint that will let us check when the transcription is complete
 polling_endpoint = make_polling_endpoint(transcript_response)
 
 # Wait until the transcription is complete
-wait_for_completion(polling_endpoint, headers)
+wait_for_completion(polling_endpoint, header)
 
 # Request the paragraphs of the transcript
-paras = get_paragraphs(polling_endpoint, headers)
+paras = get_paragraphs(polling_endpoint, header)
 
 # Print out the transcript paragraphs
 for para in paras:
