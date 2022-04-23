bb9bc8277166635acb3a82b5f1413334b7799954
Ryan O <46252299+oconnoob@users.noreply.github.com>
2022-04-23 18:47:34 -0400

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -2,13 +2,18 @@ from utils.utils import *
 
 api_key = "<YOUR-API-KEY>"
 audio_file = "audio.mp3"
+#audio_file = "https://github.com/AssemblyAI-Examples/speech-recognition-in-5-minutes-with-python/raw/main/audio.mp3"
+local = True
 
 # Create header with authorization along with AssemblyAI API requests
 header = make_header(api_key)
 
-# Upload the audio file to AssemblyAI
-upload_url = upload_file(audio_file, header)
-
+if local:
+	# Upload the audio file to AssemblyAI
+	upload_url = upload_file(audio_file, header)
+else:
+	upload_url = {'upload_url': audio_file}
+	
 # Request a transcription
 transcript_response = request_transcript(upload_url, header)
 
